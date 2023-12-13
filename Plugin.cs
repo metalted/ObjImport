e7ac88fb719166721ee135c231b2d8a6902695bc
Metalted <68029407+metalted@users.noreply.github.com>
2023-12-13 19:15:44 +0100

    Updated reader, added more settings

--- Plugin.cs
+++ Plugin.cs
@@ -14,12 +14,15 @@ namespace ObjImport
     {
         public const string pluginGUID = "com.metalted.zeepkist.objimport";
         public const string pluginName = "ObjImport";
-        public const string pluginVersion = "1.1";
-        public string pluginPath = ""; 
+        public const string pluginVersion = "1.2";
+        public string pluginPath = "";
 
         public ConfigEntry<string> objPath;
         public ConfigEntry<bool> importButton;
         public ConfigEntry<float> importScale;
+        public ConfigEntry<float> shellThickness;
+        public ConfigEntry<int> paintID;
+        public ConfigEntry<int> blockID;
 
         private void Awake()
         {
@@ -32,6 +35,10 @@ namespace ObjImport
             objPath = Config.Bind("Import", "Obj Path", "", "");
             importButton = Config.Bind("Import", "Import", true, "[Button] Import obj from path");
             importScale = Config.Bind("Import", "Import Scale", 5.0f, "");
+            shellThickness = Config.Bind("Properties", "Shell Thickness", 0.00001f, "");
+            paintID = Config.Bind("Properties", "Paint ID", 1, "");
+            blockID = Config.Bind("Properties", "Block ID", 1569, "");
+
             importButton.SettingChanged += ImportButton_SettingChanged;
             pluginPath = AppDomain.CurrentDomain.BaseDirectory + @"\BepInEx\plugins";
         }
@@ -41,7 +48,7 @@ namespace ObjImport
             //Check the file at the path.
             string filePath = (string)objPath.BoxedValue;
 
-            if(File.Exists(filePath))
+            if (File.Exists(filePath))
             {
                 // Check if the extension is .obj
                 string fileExtension = Path.GetExtension(filePath);
@@ -50,11 +57,18 @@ namespace ObjImport
                 {
                     try
                     {
-                        float scalingFactor = (float)importScale.BoxedValue;
-                        ZeepObj myObj = new ZeepObj(filePath, scalingFactor);
+                        float _importScale = (float)importScale.BoxedValue;
+                        float _shellThickness = (float)shellThickness.BoxedValue;
+                        int _blockID = (int)blockID.BoxedValue;
+                        int _paintID = (int)paintID.BoxedValue;
+
+                        ObjFile objFile = new ObjFile(filePath);
+                        objFile.ReadObj(_importScale);
+                        objFile.CreateBlockCSV(_shellThickness, _paintID, _blockID);
+
                         string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
                         string savePath = Path.Combine(pluginPath, fileNameWithoutExtension + ".zeeplevel");
-                        File.WriteAllLines(savePath, myObj.zeeplines);
+                        File.WriteAllLines(savePath, objFile.lines);
                         PlayerManager.Instance.messenger.Log("ObjImport: Imported " + fileNameWithoutExtension + "! :)", 2f);
                     }
                     catch
@@ -74,137 +88,208 @@ namespace ObjImport
         }
     }
 
-    public class ZeepObj
+    public class ObjFile
     {
         public string path;
-        public List<string> zeeplines = new List<string>();
+        public List<Vector3> vertices;
+        public List<int[]> faces;
+        public List<Triangle> triangles;
+        public List<string> lines;
 
-        public ZeepObj(string path, float scale)
+        public ObjFile(string path)
         {
             this.path = path;
+            vertices = new List<Vector3>();
+            faces = new List<int[]>();
+            triangles = new List<Triangle>();
+            lines = new List<string>();
+        }
 
-            //A list containing arrays with 3 vertices belonging to 1 triangle.
-            List<Vector3[]> triVerts = new List<Vector3[]>();
-            //A list containing vertices from the file.
-            List<Vector3> vertices = new List<Vector3>();
-            //Read the lines from the file
+        public void ReadObj(float renderScale)
+        {
+            vertices.Clear();
+            faces.Clear();
+
             string[] lines = File.ReadAllLines(path);
-
-            // First pass to collect all vertices
             foreach (string line in lines)
             {
                 string[] parts = line.Trim().Split(' ');
-
-                //Vertices
-                if (parts[0] == "v")
+                switch (parts[0])
                 {
-                    float x = float.Parse(parts[1]);
-                    float y = float.Parse(parts[2]);
-                    float z = float.Parse(parts[3]);
-                    vertices.Add(new Vector3(x, y, z) * scale);
+                    case "": //Empty Line
+                    case "#":  //Comment
+                    case "vn": //Normal
+                    case "vt": //Texture
+                    case "g": //Graphics related
+                    case "usemtl": //Material assignment
+                    case "s": //Unknown
+                        break;
+                    case "v": //Vertices
+                        //v  11.3345 20.3138 145.7801
+                        Vector3 v = new Vector3();
+                        float.TryParse(parts[1], out v.x);
+                        float.TryParse(parts[2], out v.y);
+                        float.TryParse(parts[3], out v.z);
+                        vertices.Add(v * renderScale);
+                        break;
+                    case "f": //Face
+                        int[] vertexIndices = new int[3] { 0, 0, 0 };
+                        for (int i = 0; i < 3; i++)
+                        {
+                            //f 1/1/1 2/2/1 3/3/1 
+                            //f #/./. #/./. #/./.
+
+                            string[] indices = parts[i + 1].Split('/');
+                            int.TryParse(indices[0], out vertexIndices[i]);
+                            vertexIndices[i] -= vertexIndices[i] == 0 ? 0 : 1; //Obj indices are 1 based;
+                        }
+                        faces.Add(vertexIndices);
+                        break;
                 }
             }
 
-            // Second pass to process faces
-            foreach (string line in lines)
+            CreateTriangles();
+        }
+
+        private void CreateTriangles()
+        {
+            triangles.Clear();
+            foreach (int[] face in faces)
             {
-                string[] parts = line.Trim().Split(' ');
-
-                //Faces
-                if (parts[0] == "f")
+                bool valid = true;
+                Vector3[] v_triangle = new Vector3[3];
+                for (int i = 0; i < 3; i++)
                 {
-                    int[] vertexIndices = new int[3];
-
-                    for (int i = 0; i < 3; i++)
+                    try
                     {
-                        string[] indices = parts[i + 1].Split('/');
-                        vertexIndices[i] = int.Parse(indices[0]) - 1; // OBJ indices are 1-based
+                        v_triangle[i] = vertices[face[i]];
                     }
-
-                    Vector3[] triangle = new Vector3[3];
-                    for (int i = 0; i < 3; i++)
+                    catch
                     {
-                        triangle[i] = vertices[vertexIndices[i]];
+                        valid = false;
                     }
-
-                    triVerts.Add(triangle);
                 }
-            }
 
-            //Process the triangles vertices
-            foreach (Vector3[] tvarr in triVerts)
-            {
-                List<(float, int[])> triangleSides = new List<(float, int[])> { (Vector3.Distance(tvarr[0], tvarr[1]), new int[] { 0, 1 }), (Vector3.Distance(tvarr[1], tvarr[2]), new int[] { 1, 2 }), (Vector3.Distance(tvarr[2], tvarr[0]), new int[] { 2, 0 }) };
-                //0 shortest, 2 longest
-                triangleSides.Sort((a, b) => a.Item1.CompareTo(b.Item1));
-
-                int cornerVertex = triangleSides[0].Item2.Intersect(triangleSides[1].Item2).ToArray()[0];
-                int baseVertex = triangleSides[0].Item2.Intersect(triangleSides[2].Item2).ToArray()[0];
-                int legVertex = triangleSides[1].Item2.Intersect(triangleSides[2].Item2).ToArray()[0];
-
-                bool isRightTriangle = Mathf.Approximately(Mathf.Pow(triangleSides[0].Item1, 2) + Mathf.Pow(triangleSides[1].Item1, 2), Mathf.Pow(triangleSides[2].Item1, 2));
-                if (isRightTriangle)
+                if (!valid)
                 {
-                    zeeplines.Add(CreateZeepTriangle(tvarr[cornerVertex], tvarr[baseVertex], tvarr[legVertex]));
+                    continue;
+                }
+
+                Triangle t = new Triangle(v_triangle);
+
+                if (t.IsRightTriangle())
+                {
+                    triangles.Add(t);
                 }
                 else
                 {
+                    // Vector BC
+                    Vector3 vectorBC = t._leg.position - t._base.position;
+                    // Vector AB
+                    Vector3 vectorAP = t._corner.position - t._base.position;
+                    // Project AP onto BC to find BD (vector from B to D)
+                    Vector3 vectorBD = Vector3.Project(vectorAP, vectorBC);
+                    // Point D is B + vector BD
+                    Vector3 D = t._base.position + vectorBD;
 
-                    Vector3 pointD = CalculatePointD(tvarr[cornerVertex], tvarr[baseVertex], tvarr[legVertex]);
-                    zeeplines.Add(CreateZeepTriangle(pointD, tvarr[cornerVertex], tvarr[baseVertex]));
-                    zeeplines.Add(CreateZeepTriangle(pointD, tvarr[legVertex], tvarr[cornerVertex]));
+                    triangles.Add(new Triangle(new Vector3[] { D, t._corner.position, t._base.position }));
+                    triangles.Add(new Triangle(new Vector3[] { D, t._leg.position, t._corner.position }));
                 }
             }
         }
 
-        private Vector3 CalculatePointD(Vector3 A, Vector3 B, Vector3 C)
+        public void CreateBlockCSV(float shellScale = 0.0001f, int paintID = 1, int blockID = 1569)
         {
-            // Vector BC
-            Vector3 vectorBC = C - B;
+            lines.Clear();
+            foreach (Triangle t in triangles)
+            {
+                Vector3 dirBaseCorner = t._base.position - t._corner.position;
+                Vector3 dirLegCorner = t._leg.position - t._corner.position;
+                Vector3 dirNormal = Vector3.Cross(dirLegCorner.normalized, dirBaseCorner.normalized);
 
-            // Vector AB
-            Vector3 vectorAP = A - B;
+                Vector3 position = t._base.position + dirLegCorner / 2;
+                Vector3 euler = Quaternion.LookRotation(dirLegCorner.normalized, dirNormal.normalized).eulerAngles;
+                Vector3 scale = new Vector3(-t._corner.distanceToBase / 8f, shellScale, -t._corner.distanceToLeg / 16f);
 
-            // Project AP onto BC to find BD (vector from B to D)
-            Vector3 vectorBD = Vector3.Project(vectorAP, vectorBC);
+                lines.Add($"{blockID},{position.x},{position.y},{position.z},{euler.x},{euler.y},{euler.z},{scale.x},{scale.y},{scale.z},{paintID},0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
+            }
+        }
+    }
 
-            // Point D is B + vector BD
-            Vector3 D = B + vectorBD;
+    public class Triangle
+    {
+        public Vertice _corner;
+        public Vertice _base;
+        public Vertice _leg;
 
-            return D;
+        public class Vertice
+        {
+            public enum Type { None, Corner, Base, Leg };
+            public Type type;
+
+            public Vector3 position;
+            public float distanceToBase;
+            public float distanceToLeg;
+            public float distanceToCorner;
         }
 
-        private string CreateZeepTriangle(Vector3 vCorner, Vector3 v1, Vector3 v2)
+        public Triangle(Vector3[] vertices)
         {
-            float dist_v1 = Vector3.Distance(vCorner, v1);
-            float dist_v2 = Vector3.Distance(vCorner, v2);
-            bool v1IsBase = dist_v1 < dist_v2;
+            float distance01 = Vector3.Distance(vertices[0], vertices[1]);
+            float distance12 = Vector3.Distance(vertices[1], vertices[2]);
+            float distance20 = Vector3.Distance(vertices[2], vertices[0]);
 
-            Vector3 vBase = v1IsBase ? v1 : v2;
-            Vector3 vLeg = v1IsBase ? v2 : v1;
+            Dictionary<string, float> distances = new Dictionary<string, float>()
+            {
+                {"01", distance01 },
+                {"10", distance01 },
+                {"12", distance12 },
+                {"21", distance12 },
+                {"20", distance20 },
+                {"02", distance20 },
+            };
 
-            //Calculate the base direction
-            Vector3 dirBase = vBase - vCorner;
-            //Calculate the leg direction
-            Vector3 dirLeg = vLeg - vCorner;
-            //Calculate the position of the zeep triangle
-            Vector3 position = (vBase + dirLeg / 2);
-            //Calculate the surface normal direction
-            Vector3 dirSurface = Vector3.Cross(dirLeg.normalized, dirBase.normalized);
-            //Create a rotation using the surface and leg vector.
-            Quaternion rotation = Quaternion.LookRotation(dirLeg.normalized, dirSurface.normalized);
-            //Get the euler angles
-            Vector3 euler = rotation.eulerAngles;
-            //Calculate the scale
+            (float, int[]) side01 = (distance01, new int[] { 0, 1 });
+            (float, int[]) side12 = (distance12, new int[] { 1, 2 });
+            (float, int[]) side20 = (distance20, new int[] { 2, 0 });
 
-            float baseScale = v1IsBase ? dist_v1 : dist_v2;
-            float legScale = v1IsBase ? dist_v2 : dist_v1;
+            List<(float, int[])> sides = new List<(float, int[])>()
+            {
+                side01, side12,side20
+            };
 
-            baseScale /= 8f;
-            legScale /= 16f;
+            //Sort sides by size
+            sides.Sort((a, b) => a.Item1.CompareTo(b.Item1));
 
-            //Create the string
-            return $"1569,{position.x},{position.y},{position.z},{euler.x},{euler.y},{euler.z},{-baseScale},0.0001,{-legScale},1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0";
+            //The corner is both in the shortest and middle line
+            int i_corner = sides[0].Item2.Intersect(sides[1].Item2).ToArray()[0];
+            //The base is both in the middle and the longest line
+            int i_base = sides[0].Item2.Intersect(sides[2].Item2).ToArray()[0];
+            //The leg is both in the shortest and the longest line
+            int i_leg = sides[1].Item2.Intersect(sides[2].Item2).ToArray()[0];
+
+            _corner = new Vertice();
+            _corner.type = Vertice.Type.Corner;
+            _corner.position = vertices[i_corner];
+            _corner.distanceToBase = distances[(i_corner + "" + i_base)];
+            _corner.distanceToLeg = distances[(i_corner + "" + i_leg)];
+
+            _base = new Vertice();
+            _base.type = Vertice.Type.Base;
+            _base.position = vertices[i_base];
+            _base.distanceToCorner = distances[(i_base + "" + i_corner)];
+            _base.distanceToLeg = distances[(i_base + "" + i_leg)];
+
+            _leg = new Vertice();
+            _leg.type = Vertice.Type.Leg;
+            _leg.position = vertices[i_leg];
+            _leg.distanceToCorner = distances[(i_leg + "" + i_corner)];
+            _leg.distanceToBase = distances[(i_leg + "" + i_base)];
+        }
+
+        public bool IsRightTriangle()
+        {
+            return Mathf.Approximately(Mathf.Pow(_corner.distanceToBase, 2) + Mathf.Pow(_corner.distanceToLeg, 2), Mathf.Pow(_base.distanceToLeg, 2));
         }
     }
 }
